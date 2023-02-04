e5f9689b902bb2762ee60977dce93ef87df88b68
Mateus Paiva <mateusopaiva@gmail.com>
2023-02-04 15:38:50 -0300

    Implementando o formulário

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './styles/App.css';
+import './styles/PostForm.css';
+
+import userIcon from './images/user.svg';
+import paperPlaneIcon from './images/paper-plane.svg';
 
 export default function App() {
     return (
         <div className="wrapper">
-            Math.ramdom()
+            <form className="post-form" onSubmit={() => alert('Formulario submetido')}>
+                <input placeholder="Escreva uma nova história..."/>
+                <div>
+                    <img src={userIcon} alt="User"/>
+                    <input placeholder="Digite seu nome..." />
+                    <button type="submit">
+                        <img src={paperPlaneIcon} alt="Paper plane"/>
+                        Publicar
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }

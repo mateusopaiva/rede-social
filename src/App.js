2763a18627da870e767a71b2ade12409fe9281c7
Mateus Paiva <mateusopaiva@gmail.com>
2023-02-05 17:52:03 -0300

    Feed sem itens

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import './styles/Feed.css';
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
 import clockIcon from './images/clock.svg';
+import emptyFolderIcon from './images/empty-folder.svg';
 
 export default function App() {
-    const posts = [
-        {
-            id: Math.random(),
-            content: 'Conteúdo do post',
-            userName: 'Usuário',
-            publishedAt: new Date(),
-        },
+const posts = [
         {
             id: Math.random(),
             content: 'O React é uma biblioteca criada pelo Facebook para criar elementos de interface que podem ser compartilhados e reutilizados em uma aplicação de forma rápida, simples e intuitiva.',
             userName: 'Staart',
-            publishedAt: new Date(),
+            publishedAt: new Date(), 
         },
     ];
 
-    return (
+return (
         <div className="wrapper">
             <form className="post-form" onSubmit={() => alert('Formulario submetido')}>
                 <input placeholder="Escreva uma nova história..."/>
@@ -36,27 +31,40 @@ export default function App() {
                 </div>
             </form>
         <main>
-            <header>
-                <h1>Seu Feed</h1>
-                <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-            </header>
-            <section className="feed">
-                {posts.map((post) => (
-                    <article key={post.id}>
-                        <p>{post.content}</p>
-                        <footer>
-                            <div className="user-details">
-                                <img src={userIcon} alt="User" />
-                                <strong>{post.userName}</strong>
-                            </div>
-                            <div className="time">
-                                <img src={clockIcon} alt="Clock"/>
-                                    <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
-                            </div>
-                        </footer>
-                    </article>
-                ))}
-            </section>
+            {posts.length === 0 && (
+                <div className="feed-status">
+                <img src={emptyFolderIcon} alt="Empty folder" />
+                <h1>Não encontramos nada</h1>
+                <h2>Parece que você e seus amigos não postaram nada. Comece a escrever uma nova história!</h2>
+                </div>
+            )}
+
+            {posts.length > 0 && (
+                <>
+                    <header>
+                        <h1>Seu Feed</h1>
+                        <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
+                    </header>
+
+                    <section className="feed">
+                        {posts.map((post) => (
+                            <article key={post.id}>
+                                <p>{post.content}</p>
+                                <footer>
+                                    <div className="user-details">
+                                        <img src={userIcon} alt="User" />
+                                        <strong>{post.userName}</strong>
+                                    </div>
+                                    <div className="time">
+                                        <img src={clockIcon} alt="Clock"/>
+                                            <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
+                                    </div>
+                                </footer>
+                            </article>
+                        ))}
+                    </section>
+                </>
+            )}
         </main>
         </div>
     );

d0d199d1db2e4051a3f2188505ea796133f21299
Mateus Paiva <mateusopaiva@gmail.com>
2023-02-07 19:49:35 -0300

    Propriedades no React

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './styles/Feed.css';
 
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
-import clockIcon from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
+
+import Feed from './components/Feed';
 
 export default function App() {
     const [history, setHistory] = useState('');
@@ -31,7 +31,7 @@ export default function App() {
         setUsername('');
     }
 
-return (
+    return (
         <div className="wrapper">
             <form className="post-form" onSubmit={handleSubmit}>
                 <input value={history} placeholder="Escreva uma nova história..." onChange={(event) => setHistory(event.target.value)}/>
@@ -45,40 +45,7 @@ return (
                 </div>
             </form>
         <main>
-            {posts.length === 0 && (
-                <div className="feed-status">
-                <img src={emptyFolderIcon} alt="Empty folder" />
-                <h1>Não encontramos nada</h1>
-                <h2>Parece que você e seus amigos não postaram nada. Comece a escrever uma nova história!</h2>
-                </div>
-            )}
-
-            {posts.length > 0 && (
-                <>
-                    <header>
-                        <h1>Seu Feed</h1>
-                        <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-                    </header>
-
-                    <section className="feed">
-                        {posts.map((post) => (
-                            <article key={post.id}>
-                                <p>{post.content}</p>
-                                <footer>
-                                    <div className="user-details">
-                                        <img src={userIcon} alt="User" />
-                                        <strong>{post.userName}</strong>
-                                    </div>
-                                    <div className="time">
-                                        <img src={clockIcon} alt="Clock"/>
-                                            <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
-                                    </div>
-                                </footer>
-                            </article>
-                        ))}
-                    </section>
-                </>
-            )}
+            <Feed posts={posts}/>
         </main>
         </div>
     );
